473139caf397e740cd931b3ad792e7be83f756bb
Mika Heikkinen <mika.heikkinen@iki.fi>
2022-11-18 15:15:03 +0000

    setup

---
+++ docs/mh-siemens/7c80/configs/hostmot2/flash.sh
@@ -0,0 +1,4 @@
+sudo mesaflash --device 7C80 --spi --reload --addr /dev/spidev0.0 --write 7c80d.bit
+
+#sudo mesaflash --device 7C80 --spi --reload --addr /dev/spidev0.0 --write 7c80_7i89d.bit
+
---
+++ docs/mh-siemens/7c80/flash.sh
@@ -0,0 +1,4 @@
+sudo mesaflash --device 7C80 --spi --addr /dev/spidev0.0 --write 7c80_7i74ssid.bit
+sudo mesaflash --device 7C80 --spi --addr /dev/spidev0.0 --reload
+
+
